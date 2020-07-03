8832d3872e567d04ed2d7f1aadd83bafb51bff61
Edyta Wojdala <edytawoj095@gmail.com>
2020-07-03 22:33:06 +0200

    Add new Chart

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 // function toggleMenu(visible) {
-//     document.querySelector('.menu').classList.toggle('show', visible)
-//   }
+//   document.querySelector('.menu').classList.toggle('show', visible)
+// }
 
-//   document.querySelector('.hamburger').addEventListener('click', function(e) {
-//     e.preventDefault();
-//     toggleMenu()
-//   });
+// document.querySelector('.hamburger').addEventListener('click', function(e) {
+//   e.preventDefault();
+//   toggleMenu()
+// });
 
 function closeModal() {
   document.getElementById('overlay').classList.remove('show');
@@ -24,26 +24,27 @@ document.querySelector('#overlay').addEventListener('click', function(e) {
   }
 });
 
-function openModal(modal) {
-  document.querySelectorAll('#overlay > *').forEach(function(modal) {
-    modal.classList.remove('show');
-  });
-  document.querySelector('#overlay').classList.add('show');
-  document.querySelector(modal).classList.add('show');
-}
+// function openModal(modal) {
+//   document.querySelectorAll('#overlay > *').forEach(function(modal) {
+//     modal.classList.remove('show');
+//   });
+//   document.querySelector('#overlay').classList.add('show');
+//   document.querySelector(modal).classList.add('show');
+// }
 
 var ctx = document.getElementById('myChart').getContext('2d');
 
-var chart = new Chart(ctx, {
+/*global Chart*/
+new Chart(ctx, {
   // 1
   type: 'bar',
   data: {
     // 2
-    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
     // 3
     datasets: [{
       // 4
-      label: "Signups",
+      label: 'Signups',
       // 5
       backgroundColor: '#8DBEC8',
       borderColor: '#8DBEC8',
@@ -51,13 +52,13 @@ var chart = new Chart(ctx, {
       data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
     },
     {
-      label: "FTD",
+      label: 'FTD',
       backgroundColor: '#F29E4E',
       borderColor: '#F29E4E',
       data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
     },
     {
-      label: "Earned",
+      label: 'Earned',
       backgroundColor: '#71B374',
       borderColor: '#71B374',
       data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
